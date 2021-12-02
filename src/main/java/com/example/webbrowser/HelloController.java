51dcc951efca0b3b8037336605b2b232ed4a2978
Jose <lrjose90@gmail.com>
2021-12-02 12:34:38 +0100

    prueba

---
+++ src/main/java/com/example/webbrowser/HelloController.java
@@ -0,0 +1,58 @@
+package com.example.webbrowser;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+
+    @FXML
+    private TextField adressBar;
+    @FXML
+    private Button load;
+    @FXML
+    private WebView webView;
+    private WebEngine engine;
+    String basic="http://";
+    @FXML
+
+    private Button backbutton;
+
+    public void init(){
+    adressBar.setText(basic);
+    engine = webView.getEngine();
+        adressBar.setOnKeyPressed(ke -> {
+            if (ke.getCode().equals(KeyCode.ENTER)) {
+                engine.load(adressBar.getText());
+            }
+        });
+}
+    @FXML
+    public void loadPage(ActionEvent actionEvent) {
+
+
+        engine.load(adressBar.getText());
+
+    }
+
+    @FXML
+    public void backPage(ActionEvent actionEvent) {
+        engine.getHistory().go(-1);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.webbrowser {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.webbrowser to javafx.fxml;
+    exports com.example.webbrowser;
+}
